cc88e47d89b1c5270b047f332bfe1d44237a426f
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-11 19:22:19 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/^[AEIOU]^[aeiou]/)
+  if word.match(/^[aeiou]/)
     true 
   else 
     false 
@@ -11,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+ text.scan(/w+{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
